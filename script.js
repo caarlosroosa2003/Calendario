a37395b8c3f9e6a0d2a39c6ce1419200fac9ed93
caarlosroosa2003 <119525632+caarlosroosa2003@users.noreply.github.com>
2023-10-06 16:38:57 +0200

    update

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
-let mainElement = document.querySelector("main");
+let mainElement = document.querySelector('main');
 
 const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-"Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+  "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 let monthCounter = 8;
-let currentDate = new Date(`2023-${monthCounter}-`)
+let yearCounter = 2023;
+let currentDate = new Date(`${yearCounter}-${monthCounter}-1`);
 
-function addMonth(){
-    let monthElement = document.createElement("div");
-    monthElement.classList.toggle("month");
+function addMonth() {
 
-    let titleMonthElement = document.createElement("h4");
-    titleMonthElement.innerText = monthNames[monthCounter];
+  let monthElement = document.createElement("div");
+  monthElement.classList.toggle("month");
 
-    let daysElement = document.createElement("div");
-    daysElement.classList.toggle("days");
+  let titleMonthElement = document.createElement("h4");
+  titleMonthElement.innerText = monthNames[monthCounter];
 
-    daysElement = `
-    <div class="day">L</div>
-    <div class="day">M</div>
-    <div class="day">W</div>
-    <div class="day">J</div>
-    <div class="day">V</div>
-    <div class="day">S</div>
-    <div class="day">D</div>`
+  monthElement.appendChild(titleMonthElement);
 
-    let numDaysMonth = 
+  let daysElement = document.createElement("div");
+  daysElement.classList.toggle("days");
+
+  const daysOfWeek = ["L", "M", "X", "J", "V", "S", "D"];
+
+  // INSERTAR PRIMERA FILA: L ... D
+  daysOfWeek.forEach(day => {
+    let dayElement = document.createElement("div");
+    dayElement.classList.add("day");
+    dayElement.innerText = day;
+    daysElement.appendChild(dayElement);
+  });
+
+  let numDaysMonth = getDaysInMonth(2, 2024);
+
+  for (let day = 1; day <= numDaysMonth; day++) {
+    let dayElement = document.createElement("div");
+    dayElement.classList.add("day");
+    dayElement.innerText = day;
+    daysElement.appendChild(dayElement);
+  }
+
+  monthElement.appendChild(daysElement);
+
+  mainElement.appendChild(monthElement);
+
+}
+
+function getDaysInMonth(month, year) {
+  //El día 0 es el último día del anterior mes
+  return new Date(year, month, 0).getDate();
+}
+
+let monthsOfCalendar = 9;
+for (let index = 0; index <= monthsOfCalendar; index++) {
+  addMonth();
+
 }
